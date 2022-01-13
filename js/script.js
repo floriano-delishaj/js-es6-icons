adf4b5619bbb5c8d267d61d9af4212adffad9eb5
floriano-delishaj <delishaj.floriano@live.it>
2022-01-13 11:20:36 +0100

    end

---
+++ js/script.js
@@ -0,0 +1,149 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+function gridBox (icons) {
+    return `
+    <div class="box-icons">
+        <i class="${icons.color} ${icons.family} ${icons.prefix}${icons.name}"></i>
+        <p class="">${icons.name}</p>
+    </div>
+    `
+}
+
+const containerHtml = document.getElementById('container-icons')
+
+function printIcons(container, icons) {
+
+	container.innerHTML = '';
+
+	icons.forEach((icon) => {
+
+    containerHtml.innerHTML += gridBox(icon)
+
+})
+}
+
+printIcons(containerHtml,icons)
+
+const selectIcons = document.getElementById('icons-select');
+
+selectIcons.addEventListener('change', function() {
+
+	const filterIcons = icons.filter ((icon) => {
+		return this.value === 'all' || icon.type === this.value
+	})
+	printIcons(containerHtml,filterIcons)
+
+})
